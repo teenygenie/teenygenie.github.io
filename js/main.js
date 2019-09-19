cb0b00ef04e34d3e5fc6eb55e2a58482c0bb9616
teenygenie <55385203+teenygenie@users.noreply.github.com>
2019-09-19 09:47:33 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 var stripe = Stripe('{{site.stripe_key[site.env]}}');
 
 Vue.component('shop-front', {
+  delimiters: ['${', '}'],
   props: ['products'],
 })
 
@@ -10,7 +11,6 @@ Vue.component('shop-front', {
 
 var app = new Vue({
   el: '#app',
-  delimiters: ['${', '}'],
   data: {
     cart: [],
     products: [

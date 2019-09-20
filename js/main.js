ad553739b863c26bfa8c3b10f5ec53b87c069823
teenygenie <55385203+teenygenie@users.noreply.github.com>
2019-09-20 10:43:29 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -13,9 +13,11 @@ Vue.component('store-front', {
   <div class="container">
     <div class="row">
         <div v-for = "product in products" class = "col-sm-12 col-md-6 card">
-          <img class="card-img-top">
+          <img class="card-img-top" src="img_avatar1.png" alt="Card image">
           <div class="card-body">
-            <p class="card-text">{{product.name}}</p>
+            <h4 class="card-title">{{product.name}}</h4>
+            <p class="card-text">{{product.description}}</p>
+            <a href="#" class="btn btn-primary" @clicked="add(product)">Add to cart</a>
           </div>
         </div>
       </div>

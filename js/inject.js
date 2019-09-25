9d7a13cf85fe2fef3747fc7978956894df0af3ac
teenygenie <55385203+teenygenie@users.noreply.github.com>
2019-09-25 16:25:17 +0100

    Update inject.js

--- js/inject.js
+++ js/inject.js
@@ -4,7 +4,7 @@ var stripe = Stripe('{{site.stripe_key[site.env]}}');
 var products = {{ site.data.products[site.env] | jsonify }};
 var skus = {
   {% for product in site.data.products[site.env] %}
-  {% for sku in site.data.products[site.env] %}
+  {% for sku in product.skus %}
   {% assign sku.productId = product.id %}
     {{ sku | jsonify }}
   {% endfor %}

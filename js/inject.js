112aff76ae74ec9ff86c912f7465dbd3b4ee94c6
teenygenie <55385203+teenygenie@users.noreply.github.com>
2019-09-25 16:39:32 +0100

    Update inject.js

--- js/inject.js
+++ js/inject.js
@@ -5,10 +5,10 @@ var products = {{ site.data.products[site.env] | jsonify }};
 var skus = {
   {% for product in site.data.products[site.env] %}
     {% for sku in product.skus %}
-    '{{sku.id}}':{
-          product: {{product.id}},
+    {{sku.id}}:{
+          product: '{{product.id}}',
       {% for item in sku %}
-        '{{item[0]}}':{{item[1] | jsonify}},
+        {{item[0]}}:{{item[1] | jsonify}},
       {% endfor %}
     }
     {% endfor %}

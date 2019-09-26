b6f5771a708adb5762a4e9deec9d9e1430abbced
teenygenie <55385203+teenygenie@users.noreply.github.com>
2019-09-26 15:38:25 +0100

    Update inject.js

--- js/inject.js
+++ js/inject.js
@@ -1,14 +1,19 @@
 ---
----
+---  
+{% assign _products = "" | split: "" %}
+
 const stripe = Stripe('{{site.stripe_key[site.env]}}');
 const products = {{ site.data.products[site.env] | jsonify }};
 const skus = {
-{% for product in site.data.products[site.env] %}{% for sku in product.skus %}
+{% for product in site.data.products[site.env] %}
+  {% assign _products = _products | push: product.id %}
+  {% for sku in product.skus %}
   {{sku.id}}:{
     product: '{{product.id}}',{% for item in sku %}
     {{item[0]}}:{{item[1] | jsonify}},{% endfor %}
   },{% endfor %}{% endfor %}
 };
+     {{_products | jsonify}}
 const skuPicker = [];
 {% for product in site.data.products[site.env] %}
 skuPicker['{{product.id}}'] = [];{% for sku in product.skus %}

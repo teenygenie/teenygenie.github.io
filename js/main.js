5e241625f23464dc3d7b2ce9d980ff197c1f3a9b
teenygenie <55385203+teenygenie@users.noreply.github.com>
2019-09-28 01:24:18 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -15,20 +15,20 @@ Vue.component('store-sku-picker',{
     data: function(){
         return {
             skuPicker: skuPicker,
-            colourSelection:0,
-            sizeSelection:0
+            colourIndexSelected:0,
+            sizeIndexSelected:0
         }
     },
     template: `
         <div class="btn-group" role="group">
-        <button v-for = "colour in skuPicker[this.product]" type="button" :class="{selected : this.colourSelected == colour}" class="btn btn-secondary" @click="colourSelected = colour">{{colour}}</button>
+        <button v-for = "(item, index) in skuPicker[this.product]" type="button" :class="{selected : this.colourIndexSelected == index}" class="btn btn-secondary" @click="colourIndexSelected = index">{{item[0].colour}}</button>
 
         <div class="btn-group" role="group">
         <button id="btnGroupDrop1" type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
         Size
         </button>
         <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
-        <a v-for = "size in skuPicker[this.product][this.colourSelected]" class="dropdown-item" href="#" @select="sizeSelected = size">{{size}}</a>
+        <a v-for = "(item, index) in skuPicker[this.product][this.colourIndexSelected]" class="dropdown-item" href="#" @select="sizeIndexSelected = index">{{item.size}}</a>
         </div>
         </div>
         </div>

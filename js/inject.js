d16e3983c0464bbbd7dcd56d189890b0414c0ee7
teenygenie <55385203+teenygenie@users.noreply.github.com>
2019-09-28 02:53:16 +0100

    Update inject.js

--- js/inject.js
+++ js/inject.js
@@ -13,17 +13,20 @@ const skus = {
    
 const skuPicker = {};
 {% for product in site.data.products[site.env] %}
+{% assign root = false %}
 skuPicker[{{product.id | jsonify}}] = {}
 {% assign colours = product.skus | map: "colour" | uniq %}
 {% assign sizes = product.skus | map: "size" | uniq %}
 {% for colour in colours %}
-{% assign cIndex = forloop.index0 %}
 skuPicker[{{product.id | jsonify}}][{{colour | jsonify }}] = {}
 {% for size in sizes %}
-{% assign sIndex = forloop.index0 %}
 {% for sku in product.skus %}
 {% if sku.colour == colour and sku.size == size %}
 skuPicker[{{product.id | jsonify}}][{{colour | jsonify}}][{{size | jsonify}}] = {{sku.id | jsonify}}
+{% if !root %}
+skuPicker[{{product.id | jsonify}}].root = {colour: {{colour | jsonify}}, size: {{size | jsonify}}}
+{% assign root = true %}
+{% endif %}
 {% endif %}
 {% endfor %}
 {% endfor %}

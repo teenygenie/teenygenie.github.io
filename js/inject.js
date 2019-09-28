a20170c56e5b174a51836a7cd3a3df1a44c1690f
teenygenie <55385203+teenygenie@users.noreply.github.com>
2019-09-28 02:55:24 +0100

    Update inject.js

--- js/inject.js
+++ js/inject.js
@@ -23,7 +23,7 @@ skuPicker[{{product.id | jsonify}}][{{colour | jsonify }}] = {}
 {% for sku in product.skus %}
 {% if sku.colour == colour and sku.size == size %}
 skuPicker[{{product.id | jsonify}}][{{colour | jsonify}}][{{size | jsonify}}] = {{sku.id | jsonify}}
-{% if !root %}
+{% if true %}
 skuPicker[{{product.id | jsonify}}].root = {colour: {{colour | jsonify}}, size: {{size | jsonify}}}
 {% assign root = true %}
 {% endif %}

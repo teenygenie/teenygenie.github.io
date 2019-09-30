b29a4a0e70a17e57c2f879a0b7b315812384017d
teenygenie <55385203+teenygenie@users.noreply.github.com>
2019-09-30 11:27:56 +0100

    Update inject.js

--- js/inject.js
+++ js/inject.js
@@ -2,8 +2,7 @@
 ---
 const stripe = Stripe('{{site.stripe_key[site.env]}}');
 const products = {{ site.data.products[site.env] | jsonify }};
-const skus = {
-{% for product in site.data.products[site.env] %}{% for sku in product.skus %}
+const skus = {{% for product in site.data.products[site.env] %}{% for sku in product.skus %}
   {{sku.id}}:{
     product: '{{product.id}}',{% for item in sku %}
     {{item[0]}}:{{item[1] | jsonify}},{% endfor %}
@@ -18,6 +17,7 @@ const picker =
   'colours' : [{% assign colours = product.skus | map: "colour" | uniq %}{% for colour in colours %}{% assign colourSkus = product.skus | where : 'colour', colour %}
     { 
       'colour' : {{colour | jsonify}},
+      'img' : {{colourSkus | map: "img" | first}}
       'sizes' : [{% for colourSku in colourSkus %}
         { 
           'size' : {{colourSku.size | jsonify }},
